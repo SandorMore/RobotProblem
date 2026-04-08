50dead5696631ab04818a411972f789ebcab8d24
SandorMore <msaso0725@gmail.com>
2026-04-08 22:30:08 +0200

    class created

--- robotproblem.cpp
+++ robotproblem.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
 
+class Robot {
+
+public:
+    Robot(int width, int height) {} 
+    
+};
+
 int main(int argc, const char** argv){
     for(size_t i = 1; i <= argc; ++i)
     {
