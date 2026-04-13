d98e6ee3f9682c33fcf6e03a44861f2168a8f3b5
SandorMore <msaso0725@gmail.com>
2026-04-13 17:24:34 +0200

    diabolical naming

--- robotproblem.cpp
+++ robotproblem.cpp
@@ -12,6 +12,9 @@ enum class Direction {
 
 class Cell {
 public:
+    Cell(int pos, int _pos) :x(pos), y(_pos){
+               
+    }
     int getX(){
         return this->x;
     }
@@ -41,12 +44,15 @@ public:
 private:
     int toMove = std::numeric_limits<int>::max();
     Cell currentCell;
+    Cell startPos {0, 0};
 };
 
 int main(int argc, const char** argv){
     std::vector<int> map;
+
     for(size_t i = 1; i <= argc; ++i){
         map.emplace_back(std::stoi(argv[i]));
     }
+
     return 0;
 }
